6edce3a7b916bfbec712da9ba6f5ad73680d99f6
Guillaume <guillink@gmail.com>
2017-05-20 18:50:03 +0200

    improved hundreddoors

--- hundreddoors.fsx
+++ hundreddoors.fsx
@@ -1,7 +1,7 @@
 let rec visit xs n =
-    if (n > 0 && n <= 100) then
+    match n with
+    | n when n > 0 && n <= 100 ->
         visit (List.map (fun (i,x) -> if i % n = 0 then i, not x else i, x) xs) (n+1)
-    else 
-        xs
+    | n -> xs
     
 visit (List.init 100 (fun i -> i+1, false)) 1

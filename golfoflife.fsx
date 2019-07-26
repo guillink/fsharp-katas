c78042977e021eb1b503db88f499ed33d97ec962
Guillaume L <guillink@gmail.com>
2019-07-26 17:22:32 +0200

    🪒🪒🪒 still shaving 🪒🪒🪒

--- golfoflife.fsx
+++ golfoflife.fsx
@@ -1,1 +1,1 @@
-let g(b:int[,])=Array2D.mapi(fun x y c->match ([-1,-1;-1,0;-1,1;0,-1;0,1;1,-1;1,0;1,1]|>List.sumBy(fun(x1,y1)->try b.[x+x1,y+y1]with _->0)),c with 2,1->1|3,_->1|_->0)b
+let g(b:int[,])=Array2D.mapi(fun x y c->match ([-1,-1;-1,0;-1,1;0,-1;0,1;1,-1;1,0;1,1]|>List.sumBy(fun(i,j)->try b.[x+i,y+j]with _->0)),c with 2,1|3,_->1|_->0)b
